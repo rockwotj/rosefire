07ebd1da653774ff82de0d03130915170a6ec478
rockwotj <rockwotj@gmail.com>
2015-09-26 16:28:01 -0400

    Intial Commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Rose-Hulman Firebase Authentication
+
+This is a simple service that authenticates Rose-Hulman students via Kerberos Login and returns a [Firebase Custom Auth Token](https://www.firebase.com/docs/web/guide/login/custom.html).
+
+This README and documentation is a work in progress
+
+## Production Use
+
+Endpoint details
+
+client library details
+
+## Production Setup
+
+This is a simple nodejs app that is reverse proxied by nginx. 
+
+## Development
+
+blah
---
+++ index.js
@@ -0,0 +1,67 @@
+var ActiveDirectory = require('activedirectory');
+var FirebaseTokenGenerator = require("firebase-token-generator");
+var Express = require('express');
+var BodyParser = require('body-parser');
+var moment = require('moment');
+
+var ldapConfig = {url: 'ldap://rose-hulman.edu:389'}
+var ad = new ActiveDirectory(ldapConfig);
+
+var extractEmailUsername = function(email) {
+  var emailSplit = email.split("@");
+  return emailSplit[0];
+}
+
+var app = Express();
+// Middleware for JSON encoded post & put requests
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: false }));
+
+app.use(function (req, res, next) {
+  var email = req.body.email;
+  var password = req.body.password;
+  var secret = req.body.secret;
+  if(!email || !password ) {
+    res.status(400).json({error: {message: 'Missing email or password in request'}, status: 400});
+  } else if (!secret) {
+    res.status(400).json({error: {message: 'Missing firebase secret in request'}, status: 400});
+  } else {
+    next();
+  }
+});
+
+app.post('/api/auth', function(req, res) {
+  var email = req.body.email;
+  var password = req.body.password;
+  var secret = req.body.secret;
+  var tokenOptions = req.body.options;
+  ad.authenticate(email, password, function(err, auth) {
+    if (err || !auth) {
+      console.log(email + " failed authentication!");
+      res.status(400).json({error:err || {message: 'Bad client credentials'}, status: 400});
+      return; 
+    }
+    var tokenGenerator = new FirebaseTokenGenerator(secret); 
+    var username = extractEmailUsername(email);
+    console.log(username + " has authenticated!");
+    var tokenData = {
+      uid: username, 
+      email:email, 
+      domain:"rose-hulman.edu", 
+      timestamp: moment().format()
+    };
+    var token = tokenGenerator.createToken(tokenData, tokenOptions);
+    console.log("Generated token: ");
+    console.log(token);
+    res.json({token: token, timestamp: tokenData.timestamp, username: tokenData.uid});
+  });
+
+});
+
+var port = 8080;
+var ip_address = '127.0.0.1';
+
+var server = app.listen(port, ip_address, function () {
+    console.log('Example app listening at http://localhost:%s', port);
+});
+

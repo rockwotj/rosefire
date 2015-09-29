302885369e13b3ecbff0dab0ca785f7d0d84db4e
rockwotj <rockwotj@gmail.com>
2015-09-29 02:11:55 -0400

    Added the register endpoint

--- index.js
+++ index.js
@@ -3,59 +3,104 @@ var FirebaseTokenGenerator = require("firebase-token-generator");
 var Express = require('express');
 var BodyParser = require('body-parser');
 var moment = require('moment');
+var jwt = require('jsonwebtoken');
+var fs = require('fs');
 
 var ldapConfig = {url: 'ldap://rose-hulman.edu:389'}
-var ad = new ActiveDirectory(ldapConfig);
+var rose = new ActiveDirectory(ldapConfig);
+
+var secrets = JSON.parse(fs.readFileSync('secrets.json'));
 
 var extractEmailUsername = function(email) {
   var emailSplit = email.split("@");
   return emailSplit[0];
-}
+};
 
 var app = Express();
 
 app.use(BodyParser.json());
-app.use(BodyParser.urlencoded({ extended: false }));
+app.use(BodyParser.urlencoded({extended: false}));
 
 app.use(function (req, res, next) {
   var email = req.body.email;
   var password = req.body.password;
-  var secret = req.body.secret;
   if(!email || !password ) {
     res.status(400).json({error: {message: 'Missing email or password in request'}, status: 400});
-  } else if (!secret) {
+  } else {
+    rose.authenticate(email, password, function(err, auth) {
+      if (err || !auth) {
+        console.log(email + " failed authentication!");
+        res.status(400).json({error: err.toString(), status: 400});
+        return; 
+      }
+      next();
+    });
+  }
+});
+
+app.use('/api/auth', function (req, res, next) {
+  var token = req.body.registryToken;
+  if (!token) {
+    res.status(400).json({error: {message: 'Missing registryToken in request'}, status: 400});
+  } else {
+    jwt.verify(token, secrets.key, secrets, function(err, decoded) {
+      if(err) {
+        res.status(400).json({error: {message: 'Bad registryToken in request'}, status: 400});
+        return;
+      }
+      console.log("Registry token successfully decoded");
+      console.log(decoded);
+      req.body.secret = decoded.admin;
+      req.body.admin = decoded.admin;
+      next();
+    });
+  }
+});
+
+app.post('/api/auth', function(req, res) {
+  var email = req.body.email;
+  var username = extractEmailUsername(email);
+  var password = req.body.password;
+  var secret = req.body.secret;
+  var tokenOptions = req.body.options || {};
+  tokenOptions.debug = false;
+  tokenOptions.admin = admin === username;
+
+  var tokenGenerator = new FirebaseTokenGenerator(secret); 
+  var tokenData = {
+    uid: username, 
+    email: email, 
+    domain: "rose-hulman.edu", 
+    timestamp: moment().format()
+  };
+  var token = tokenGenerator.createToken(tokenData, tokenOptions);
+  console.log("Generated token authenticating " + username);
+  res.json({token: token, timestamp: tokenData.timestamp, username: tokenData.uid});
+});
+
+app.use('/api/register', function (req, res, next) {
+  var secret = req.body.secret;
+  if (!secret) {
     res.status(400).json({error: {message: 'Missing firebase secret in request'}, status: 400});
   } else {
     next();
   }
 });
 
-app.post('/api/auth', function(req, res) {
+app.post('/api/register', function(req, res, next) {
   var email = req.body.email;
+  var username = extractEmailUsername(email);
   var password = req.body.password;
   var secret = req.body.secret;
-  var tokenOptions = req.body.options;
-  ad.authenticate(email, password, function(err, auth) {
-    if (err || !auth) {
-      console.log(email + " failed authentication!");
-      res.status(400).json({error: err.toString(), status: 400});
-      return; 
-    }
-    var tokenGenerator = new FirebaseTokenGenerator(secret); 
-    var username = extractEmailUsername(email);
-    console.log(username + " has authenticated!");
-    var tokenData = {
-      uid: username, 
-      email: email, 
-      domain: "rose-hulman.edu", 
-      timestamp: moment().format()
-    };
-    var token = tokenGenerator.createToken(tokenData, tokenOptions);
-    console.log("Generated token: ");
-    console.log(token);
-    res.json({token: token, timestamp: tokenData.timestamp, username: tokenData.uid});
-  });
+  var tokenData = {
+    admin: username,
+    secret: secret,
+    timestamp: moment().format()
+  };
 
+  var token = jwt.sign(tokenData, secrets.key, secrets);
+  console.log("Generated registryToken for " + username);
+  res.json({registryToken: token, timestamp: tokenData.timestamp, username: username}); 
 });
 
 app.use(function(err, req, res, next) {

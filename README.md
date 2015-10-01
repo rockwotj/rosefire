5c6daf976c4b1e7d93a000cb4b4210cf1c1588ac
rockwotj <rockwotj@gmail.com>
2015-09-30 22:46:05 -0400

    Initial Test Release

---
+++ README.md
@@ -0,0 +1,126 @@
+# Rose-Hulman Firebase Authentication
+
+This is a simple service that authenticates Rose-Hulman students via Kerberos Login and returns a [Firebase Custom Auth Token](https://www.firebase.com/docs/web/guide/login/custom.html).
+
+This README and documentation is a work in progress
+
+## Production Use
+
+Tokens recieved from this service will have the following fields, which can accessed in your firebase rules. More information about this can be found in [Firebase's Custom Auth Documentation](https://www.firebase.com/docs/web/guide/login/custom.html).
+
+```json
+uid: (String) the Rose username of the user
+email: (String) the email of the user
+domain: (String) "rose-hulman.edu"
+timestamp: (String) an ISO formatted timestamp of when it was created.
+```
+
+### Endpoints
+
+The hostname for this service has yet to be determined, but will be behind Rose-Hulman's Firewall. These endpoints also support [jsonp](https://en.wikipedia.org/wiki/JSONP) requests for javascript clients by adding a 'callback' parameter to the query string. For example, `/api/register/` → `/api/register/?callback=foo`
+
+#### POST `/api/register/`
+
+A post request to `/api/register/` requires the following parameters in the JSON body of the request. The email and password is who the project will be registered under. 
+
+```json
+{
+  "email": "rockwotj@rose-hulman.edu",
+  "password": "Pas$w0rd", 
+  "secret": "<FIREBASE SECRET HERE>"
+}
+```
+
+This will then return a JSON object like the following.
+
+```json
+{
+  "username": "rockwotj",
+  "timestamp": "<ISO formatted timestamp>", 
+  "registryToken": "<YOUR REGISTRY TOKEN HERE>"
+}
+```
+
+Note: When the person who the project is registered under is authenticated via the `/api/auth/` endpoint, they have full read-write access to the entire firebase repo by default. This can be disabled by explictly setting admin to false in the `/api/auth/` endpoint. No one else is allowed to have admin access for security reasons.
+
+#### POST `/api/auth/`
+
+A post request to `/api/auth/` requires the following parameters in the JSON body of the request. The options field and all of that object's fields are completely optional and not required.
+
+```json
+{
+  "email": "user@rose-hulman.edu",
+  "password": "Pas$w0rd", 
+  "registryToken": "<REGISTRY TOKEN HERE>",
+  "options": {
+    "expires": 12340192830,
+    "notBefore": 1234234134,
+    "admin": false
+  }
+}
+```
+
+These are the options for the endpoint, more information can be found [here](https://github.com/firebase/firebase-token-generator-node#token-options).
+
+* <b>expires</b>: (Integer) A timestamp of when the token is invalid.
+* <b>notBefore</b>: (Integer) A timestamp of when the token should start being valid.
+* <b>admin</b>: (Boolean) If true, then all security rules are disabled for this user. This can only be true for the user who the token is registred with.
+
+This endpoint returns an object that looks like this
+
+```json
+{
+  "token": "<Auth token for user@rose-hulman.edu>",
+  "timestamp": "<ISO formatted timestamp>",
+  "username": "user"
+}
+```
+
+#### Endpoint Errors
+
+If there is an error during a request for any endpoint then the response will look like this
+
+```json
+{
+  "error": "error message",
+  "status": "HTTP Status code of the error"
+}
+```
+
+### Client Libraries
+
+In the near future there will be client libraries available to more easily integrate this into your code.
+
+#### Java
+
+TODO
+
+#### Swift
+
+TODO
+
+#### Javascript
+
+TODO
+
+
+## Production Setup
+
+This is a simple nodejs app that is reverse proxied by nginx. Here are the deployment instructions:
+
+```
+git clone https://github.com/rockwotj/rose-firebase-auth.git/
+npm install
+./scripts/start
+```
+
+Make sure nginx is set up over HTTPS to proxy to localhost:8080. The nginx configuration is included below.
+
+### Secrets File
+
+In order to run this server, a `secrets.json` file is required. At minimum, it must have a 'key' field that will be used as a symmetric key for the JWT that is the registry token. It may also include the following fields: 'subject', 'issuer', and 'audience'.
+
+### Nginx Configuration 
+
+TODO
+
---
+++ rosefirebaseauth/src/main/java/edu/rosehulman/rockwotj/rosefirebaseauth/RoseFirebaseAuth.java
@@ -0,0 +1,174 @@
+package edu.rosehulman.rockwotj.rosefirebaseauth;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.firebase.client.Firebase;
+import com.firebase.client.Firebase.AuthResultHandler;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Map;
+
+public class RoseFirebaseAuth {
+
+    public static final String TAG = "RFA";
+
+    private final String mRoseAuthServiceUrl;
+    private final Firebase mFirebaseRef;
+    private final String mRegistryToken;
+
+    public RoseFirebaseAuth(Firebase repo, String registryToken) {
+        // Default to localhost on emulator's port 8080.
+        this(repo, registryToken, "http://10.0.0.2:8080");
+    }
+
+    public RoseFirebaseAuth(Firebase repo, String registryToken, String authServiceUrl) {
+        mFirebaseRef = repo;
+        mRegistryToken = registryToken;
+        mRoseAuthServiceUrl = authServiceUrl + "/api/";
+    }
+
+    public void authWithRoseHulman(String email, String password, AuthResultHandler handler) {
+        authWithRoseHulman(email, password, handler, null);
+    }
+
+    public void authWithRoseHulman(String email, String password, AuthResultHandler handler, TokenOptions options) {
+        new RoseTokenFetcher(email, password, handler, options).execute();
+    }
+
+    public class TokenOptions {
+        private Integer expires;
+        private Integer notBefore;
+        private Boolean admin;
+
+        public TokenOptions() {
+            this(null, null, null);
+        }
+
+        public TokenOptions(Integer expires, Integer notBefore, Boolean admin) {
+            this.expires = expires;
+            this.notBefore = notBefore;
+            this.admin = admin;
+        }
+
+        public Integer getExpires() {
+            return expires;
+        }
+
+        public void setExpires(Integer expires) {
+            this.expires = expires;
+        }
+
+        public Integer getNotBefore() {
+            return notBefore;
+        }
+
+        public void setNotBefore(Integer notBefore) {
+            this.notBefore = notBefore;
+        }
+
+        public void setAdmin(Boolean admin) {
+            this.admin = admin;
+        }
+
+        public Boolean isAdmin() {
+            return admin;
+        }
+    }
+
+    private class RoseTokenFetcher extends AsyncTask<Void, Void, String> {
+
+        private final String mEmail;
+        private final String mPassword;
+        private final AuthResultHandler mResultHandler;
+        private final TokenOptions mOptions;
+
+        public RoseTokenFetcher(String email, String password, AuthResultHandler handler, TokenOptions options) {
+            mEmail = email;
+            mPassword = password;
+            mResultHandler = handler;
+            mOptions = options;
+        }
+
+        @Override
+        protected String doInBackground(Void... ignored) {
+            ObjectMapper mapper = new ObjectMapper();
+            ObjectNode params = mapper.createObjectNode()
+                .put("email", mEmail)
+                .put("password", mPassword)
+                .put("registryToken", mRegistryToken);
+            if (mOptions != null) {
+                params.putPOJO("options", mOptions);
+            }
+            Log.d(TAG, params.toString());
+            String response = makeRequest("auth", params.toString());
+            if (response == null || response.isEmpty()) {
+                return null;
+            }
+            Map<String, String> data;
+            try {
+                data = mapper.readValue(response, Map.class);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+            Log.d(TAG, "Authentation for " + data.get("username"));
+            return data.get("token");
+        }
+
+        @Override
+        protected void onPostExecute(String roseAuthToken) {
+            mFirebaseRef.authWithCustomToken(roseAuthToken, mResultHandler);
+        }
+    }
+
+    private String makeRequest(String endpoint, String json) {
+        HttpURLConnection urlConnection;
+        String url = mRoseAuthServiceUrl + endpoint + "/";
+        String data = json;
+        String result = null;
+        try {
+            urlConnection = (HttpURLConnection) ((new URL(url).openConnection()));
+            urlConnection.setDoOutput(true);
+            urlConnection.setRequestProperty("Content-Type", "application/json");
+            urlConnection.setRequestProperty("Accept", "application/json");
+            urlConnection.setRequestMethod("POST");
+            urlConnection.connect();
+
+            //TODO: Use com.fasterxml.jackson for serialization instead of bufferedReader/Writer
+            OutputStream outputStream = urlConnection.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+            writer.write(data);
+            writer.close();
+            outputStream.close();
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
+
+            String line;
+            StringBuilder sb = new StringBuilder();
+
+            while ((line = bufferedReader.readLine()) != null) {
+                sb.append(line);
+            }
+
+            bufferedReader.close();
+            result = sb.toString();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+}

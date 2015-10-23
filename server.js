e21dd49e51697d261d1e70d6ecba912d7ae60bda
Tyler Rockwood <rockwotj@users.noreply.github.com>
2015-10-23 13:15:58 -0400

    Moved CORS to be for registration also

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use(function (req, res, next) {
   }
 });
 
-app.use('/api/auth', function(req, res, next) {
+app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();

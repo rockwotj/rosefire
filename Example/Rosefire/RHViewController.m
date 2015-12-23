978c13e6cddc509768f936700616fee0486d5102
rockwotj <rockwotj@gmail.com>
2015-12-23 12:51:29 -0800

    Added example for pods

--- Example/Rosefire/RHViewController.m
+++ Example/Rosefire/RHViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "RHViewController.h"
+#import "Firebase.h"
+#import "Rosefire.h"
 
 @interface RHViewController ()
 
@@ -17,6 +19,19 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    Firebase* myFirebaseRef = [[Firebase alloc] initWithUrl:@"https://myproject.firebaseio.com"];
+    [myFirebaseRef authWithRoseHulman:@"<REGISTRY_TOKEN>"
+                                email:@"rockwotj@rose-hulman.edu"
+                             password:@"Pa$sW0rd"
+                  withCompletionBlock:^(NSError * err, FAuthData * authData) {
+                      if (!err) {
+                          // Show logged in UI
+                          NSLog(@"Logged in!");
+                      } else {
+                          // Show login error
+                          NSLog(@"Not logged in :(");
+                      }
+                  }];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 

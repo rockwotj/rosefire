dab3588b4d874b25d01ff795ed3b493f64c1d96d
Tyler Rockwood <rockwotj@users.noreply.github.com>
2015-12-23 12:13:30 -0800

    Updated CORS headers

--- server.js
+++ server.js
@@ -49,6 +49,7 @@ app.use(function (req, res, next) {
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "POST, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });

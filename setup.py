4fe878014ba5bd88de4a25add0c1046f5f778c83
rockwotj <rockwotj@gmail.com>
2015-12-24 16:48:14 -0800

    Readding setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='rosefire',
+    version='1.0.0',
+    author='Tyler Rockwood',
+    author_email='rockwotj@rose-hulman.edu',
+    zip_safe=False,
+    py_modules=['rosefire'],
+    license='MIT',
+    url='https://github.com/rockwotj/rosefire/tree/python-server',
+    description='A utility to verify Rosefire tokens',
+    classifiers=[
+       'Programming Language :: Python',
+       'Programming Language :: Python :: 2',
+       'Programming Language :: Python :: 3',
+    ]
+)

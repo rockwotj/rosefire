8bb67800b1c080ac739fcbf1a5771757b61a02c8
Tyler Rockwood <rockwotj@gmail.com>
2016-03-26 00:00:21 -0400

    Initial attempt at v2

--- rosefire.js
+++ rosefire.js
@@ -1,48 +1,31 @@
 (function(exports) {
-  var createCORSRequest = function(method, url) {
-    var xhr = new XMLHttpRequest();
-    if ("withCredentials" in xhr) {
-      xhr.open(method, url, true);
-    } else if (typeof XDomainRequest != "undefined") {
-      xhr = new XDomainRequest();
-      xhr.open(method, url);
-    } else {
-      // Otherwise, CORS is not supported by the browser.
-      xhr = null;
-    }
-    return xhr;
-  };
-  var getRosefireToken = function(data, callback) {
-    xhr = createCORSRequest('POST', 'https://rosefire.csse.rose-hulman.edu/api/auth');
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.onload = function() {
-      if (xhr.status === 200) {
-        var response = JSON.parse(xhr.responseText);
-        callback(null, response.token);
-      } else if (xhr.responseText) {
-        callback(new Error(xhr.responseText));
-      } else {
-        callback(new Error("Error authenticating"));
+  var getRosefireToken = function(registryToken, callback) {
+    var token = encodeURIComponent(registryToken);
+    var origin = encodeURIComponent(location.origin);
+    var rosefireWindow = window.open('https://rosefire.csse.rose-hulman.edu/webview/login?platform=web&registryToken=' + token + '&referrer=' + origin, '_blank');
+    rosefireWindow.onclose = function() {
+      if (callback) {
+        callback(new Error('Login cancelled'));
+        callback = null;
       }
     };
-    xhr.onerror = function() {
-      callback(new Error("Error making request to Rosefire"));
-    };
-    xhr.send(JSON.stringify(data));
+    window.addEventListener('message', function(event) {
+      var origin = event.origin || event.originalEvent.origin;
+      if (origin !== 'https://rosefire.csse.rose-hulman.edu') {
+        console.error('Invalid origin:' + origin);
+        return;
+      }
+      var cb = callback;
+      callback = null;
+      event.source.close();
+      if (cb) {
+        cb(null, event.data);                     
+      }
+    });
   };
   if (typeof Firebase !== "undefined") {
-    Firebase.prototype.authWithRoseHulman = function(registryToken, email, password, options, callback) {
-      var data = {
-        registryToken: registryToken,
-        email: email,
-        password: password
-      };
-      if (callback) {
-        data.options = options;
-      } else {
-        callback = options;
-      }
-      getRosefireToken(data, function(err, token) {
+    Firebase.prototype.authWithRoseHulman = function(registryToken, callback) {
+      getRosefireToken(registryToken, function(err, token) {
         if (err) {
           callback(err);
         } else {

35b2e26134ca05e787cfb3a7d1f038a8d54c2792
Tyler Rockwood <rockwotj@gmail.com>
2016-03-26 10:08:43 -0400

    Got error handling to work on window close

--- rosefire.js
+++ rosefire.js
@@ -3,12 +3,16 @@
     var token = encodeURIComponent(registryToken);
     var origin = encodeURIComponent(location.origin);
     var rosefireWindow = window.open('https://rosefire.csse.rose-hulman.edu/webview/login?platform=web&registryToken=' + token + '&referrer=' + origin, '_blank');
-    rosefireWindow.onclose = function() {
-      if (callback) {
-        callback(new Error('Login cancelled'));
-        callback = null;
+    // Hacky, but seems to be the only method.
+    var intervalId = setInterval(function() {
+      if (rosefireWindow.closed) {
+          clearInterval(intervalId);
+          if (callback) {
+            callback(new Error('Login cancelled'));
+            callback = null;
+          }
       }
-    };
+    }, 500);
     window.addEventListener('message', function(event) {
       var origin = event.origin || event.originalEvent.origin;
       if (origin !== 'https://rosefire.csse.rose-hulman.edu') {

0ef679f421b330763dfc19319d44e3c72f887cab
Tyler Rockwood <rockwotj@gmail.com>
2016-03-28 12:09:31 -0400

    Made methods static

--- rosefire/src/main/java/edu/rosehulman/rosefire/Rosefire.java
+++ rosefire/src/main/java/edu/rosehulman/rosefire/Rosefire.java
@@ -9,13 +9,13 @@ public class Rosefire {
 
     static boolean DEBUG = false;
 
-    public Intent getSignInIntent(Context context, String registryToken) {
+    public static Intent getSignInIntent(Context context, String registryToken) {
         Intent intent = new Intent(context, WebLoginActivity.class);
         intent.putExtra(WebLoginActivity.REGISTRY_TOKEN, registryToken);
         return intent;
     }
 
-    public String getSignInResultFromIntent(Intent data) {
+    public static String getSignInResultFromIntent(Intent data) {
         return data.getStringExtra(WebLoginActivity.JWT_TOKEN);
     }
 

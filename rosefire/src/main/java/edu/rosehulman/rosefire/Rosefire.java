5c46710b5969add766bee39b02dce75cd57a1844
Tyler Rockwood <rockwotj@gmail.com>
2016-03-28 12:04:21 -0400

    Simplified API

--- rosefire/src/main/java/edu/rosehulman/rosefire/Rosefire.java
+++ rosefire/src/main/java/edu/rosehulman/rosefire/Rosefire.java
@@ -1,6 +1,6 @@
 package edu.rosehulman.rosefire;
 
-import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 
 public class Rosefire {
@@ -8,34 +8,14 @@ public class Rosefire {
     public static int REQUEST_CODE = 5500;
 
     static boolean DEBUG = false;
-    private Activity activity;
 
-    private static Rosefire instance;
-
-    public static Rosefire getInstance() {
-        if (instance == null) {
-            instance = new Rosefire();
-        }
-        return instance;
-    }
-
-    private Rosefire() {
-    }
-
-    public void setActivity(Activity activity) {
-        this.activity = activity;
-    }
-
-    public void signIn(String registryToken) {
-        if (activity == null) {
-            throw new RuntimeException("You MUST set the activity via `setActivity` before you can call `signIn`");
-        }
-        Intent intent = new Intent(activity, WebLoginActivity.class);
+    public Intent getSignInIntent(Context context, String registryToken) {
+        Intent intent = new Intent(context, WebLoginActivity.class);
         intent.putExtra(WebLoginActivity.REGISTRY_TOKEN, registryToken);
-        activity.startActivityForResult(intent, REQUEST_CODE);
+        return intent;
     }
 
-    public String extractToken(Intent data) {
+    public String getSignInResultFromIntent(Intent data) {
         return data.getStringExtra(WebLoginActivity.JWT_TOKEN);
     }
 
